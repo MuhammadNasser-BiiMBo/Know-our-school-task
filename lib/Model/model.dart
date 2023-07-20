b1da3c1520c5afe7335b729811d801524c15cbda
NasserBiMBo <1900596@eng.asu.edu.eg>
2023-07-20 21:27:07 +0300

    Task Successfully Completed

---
+++ lib/Model/model.dart
@@ -0,0 +1,16 @@
+//TODO: add your model class here
+class School{
+   String? schoolName;
+   String? schoolAddress;
+   String? numberOfStudents;
+   String? numberOfTeachers;
+
+  School(this.schoolName, this.schoolAddress, this.numberOfStudents, this.numberOfTeachers);
+  //to get the data from the json object and fill the class attributes with it.
+   School.fromJson(Map <String,dynamic>json){
+      schoolName = json['School Name'];
+      schoolAddress = json['School Address'];
+      numberOfStudents = json['Number of Students'];
+      numberOfTeachers = json['Number of Teachers'];
+  }
+}
